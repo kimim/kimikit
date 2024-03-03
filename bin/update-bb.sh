2a970edb4ac675472381f073fda25fcfb27ee019
kimim <kimi.im@outlook.com>
2024-03-03 08:48:15 +0800

    bb cannot use bb task to overwrite itself

---
+++ bin/update-bb.sh
@@ -0,0 +1,3 @@
+#!sh
+curl -L --output-dir /kimikit/tmp -O https://github.com/babashka/babashka/releases/download/v1.3.189/babashka-1.3.189-windows-amd64.zip
+unzip -o babashka-1.3.189-windows-amd64.zip -d /kimikit/babashka
