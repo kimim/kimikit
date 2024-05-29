0bd076b7449feac96deae56fe345585eaa3768a1
Kimi Ma <kimi.im@outlook.com>
2024-05-29 12:57:00 +0800

    fix path

--- bin/emacs.bat
+++ bin/emacs.bat
@@ -5,4 +5,4 @@ rem change home path by adding below line
 set HOME=%USERPROFILE%\home
 set MSYS_ROOT=%USERPROFILE%\msys64
 set MSYS2_PATH_TYPE=strict
-%MSYS_ROOT%\\usr\\bin\\env MSYSTEM=UCRT64 /usr/bin/bash -lc "source /etc/bash.bashrc && emacs"
+%MSYS_ROOT%\\usr\\bin\\env MSYSTEM=UCRT64 /usr/bin/bash -lc "source $HOME/.bashrc && emacs"
